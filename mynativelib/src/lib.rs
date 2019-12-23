09f9c3f01a433e6fe2f22d08bdc6448897ffba7f
Andrew Duffy <andreweduffy@gmail.com>
2019-12-23 13:14:25 -0500

    Remove an expected warning

--- mynativelib/src/lib.rs
+++ mynativelib/src/lib.rs
@@ -3,6 +3,7 @@ use jni::objects::JClass;
 use jni::sys::jstring;
 
 #[no_mangle]
+#[allow(unused_variables)]
 pub extern "system" fn Java_FFITest_greet(env: JNIEnv, class: JClass) -> jstring {
     // Create a new JString object
     env.new_string("hello world!")
